eca8bca19fe74ddbc0f7989465749daa703b2458
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-09-29 21:10:16 +0200

    supposedly working code based on System.Drawing.Text.

--- WinFontSync.Host/Program.cs
+++ WinFontSync.Host/Program.cs
@@ -1,7 +1,42 @@
 using System.Drawing;
 using System.Drawing.Text;
+using System.Runtime.InteropServices;
 
+string fontDirectory                            = "C:\\Users\\HMO\\OneDrive - INGTES AG\\Desktop\\Fonts";
 InstalledFontCollection installedFontCollection = new();
+PrivateFontCollection privateFontCollection     = new();
+int nbrOfInstalledFonts                         = privateFontCollection.Families.Length;
+IEnumerable<string> fontFilePaths               = Directory
+                                                    .GetFiles(fontDirectory)
+                                                    .Where(fp => fp.EndsWith(".ttf") || fp.EndsWith(".otf"));
+
+foreach (string fontFilePath in fontFilePaths)
+{
+  privateFontCollection.AddFontFile(fontFilePath);
+
+  if (privateFontCollection.Families.Length > nbrOfInstalledFonts)
+  {
+    nbrOfInstalledFonts = privateFontCollection.Families.Length;
+
+    byte[] fontData = File.ReadAllBytes(fontFilePath);
+    IntPtr fontPtr  = Marshal.AllocCoTaskMem(fontData.Length);
+
+    // Installing the font
+    Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+    privateFontCollection.AddMemoryFont(fontPtr, fontData.Length);
+    Marshal.FreeCoTaskMem(fontPtr);
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Font installed successfully: '{fontFilePath}'");
+    Console.ForegroundColor = ConsoleColor.White;
+  }
+  else
+  {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Failed to install the font file: '{fontFilePath}'");
+    Console.ForegroundColor = ConsoleColor.White;
+  }
+}
 
 foreach (FontFamily font in installedFontCollection.Families)
 {
